95c0c52ed040b0b67517c9656d97a966a0174563
Lei <lynnl.yet@gmail.com>
2019-04-14 13:20:53 +0800

    Add template code

---
+++ kext/bsd_kext_log.c
@@ -0,0 +1,18 @@
+/*
+ * Created 190414 lynnl
+ */
+
+#include <mach/mach_types.h>
+
+kern_return_t bsd_kext_log_start(kmod_info_t *ki, void *d)
+{
+
+    return KERN_SUCCESS;
+}
+
+kern_return_t bsd_kext_log_stop(kmod_info_t *ki, void *d)
+{
+
+    return KERN_SUCCESS;
+}
+
