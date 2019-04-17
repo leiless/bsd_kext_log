76e607787228149b7adc1fc09d7f0503fba72c33
Lei <lynnl.yet@gmail.com>
2019-04-17 23:49:57 +0800

    Add KMOD_EXPLICIT_DECL info
    Otherwise kextload will prompt you:
    
    /tmp/bsd_kext_log.kext is invalid; can't resolve dependencies.
    
    cn.junkman.kext.bsd_kext_log's dependencies failed security checks; failing.

--- kext/bsd_kext_log.c
+++ kext/bsd_kext_log.c
@@ -41,3 +41,21 @@ kern_return_t bsd_kext_log_stop(kmod_info_t *ki, void *d)
     return r;
 }
 
+#ifdef __kext_makefile__
+extern kern_return_t _start(kmod_info_t *, void *);
+extern kern_return_t _stop(kmod_info_t *, void *);
+
+/* Will expand name if it's a macro */
+#define KMOD_EXPLICIT_DECL2(name, ver, start, stop) \
+    __attribute__((visibility("default")))          \
+        KMOD_EXPLICIT_DECL(name, ver, start, stop)
+
+KMOD_EXPLICIT_DECL2(BUNDLEID, KEXTBUILD_S, _start, _stop)
+
+/* If you intended to write a kext library  NULLify _realmain and _antimain */
+__private_extern__ kmod_start_func_t *_realmain = bsd_kext_log_start;
+__private_extern__ kmod_stop_func_t *_antimain = bsd_kext_log_stop;
+
+__private_extern__ int _kext_apple_cc = __APPLE_CC__;
+#endif
+
